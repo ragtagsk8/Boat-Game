1bbfedf8cff01c35038b4219859784f27604627e
ragtagsk8 <kanearriagada06@gmail.com>
2025-06-14 13:48:16 +1000

    Create Commit
    Created basic objects and scripts

---
+++ My project/Assets/Scripts/Cursor.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Cursor : MonoBehaviour
+{
+    private Transform mouseTrans;
+
+    void Start()
+    {
+        //mouseTrans = 
+    }
+
+    void Update()
+    {
+        //transform.position = mouseTrans.position;
+    }
+}
