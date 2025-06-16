a4b5a12f9cebd645d9b29a6eb8976408a939faac
ragtagsk8 <kanearriagada06@gmail.com>
2025-06-16 16:28:48 +1000

    Boat Scripting Start
    Started to script the boat, just creating basic variables to call reference to the Unity features like collider and rigidbody.

--- My project/Assets/Scripts/Boat.cs
+++ My project/Assets/Scripts/Boat.cs
@@ -2,15 +2,44 @@ using UnityEngine;
 
 public class Boat : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Rigidbody2D rb;
+    private CapsuleCollider2D coll;
+    private SpriteRenderer sprite;
+
+    //MOVEMENT
+    private bool anchored;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        coll = GetComponent<CapsuleCollider2D>();
+        sprite = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Anchor();
+        if (anchored == false)
+        {
+            Movement();
+        }
+    }
+
+    private void Movement()
+    {
+
+    }
+
+    private void Anchor()
+    {
+        if (Input.GetKeyDown("a") && anchored == false)
+        {
+            anchored = true;
+        }
+        if (Input.GetKeyDown("a") && anchored == true)
+        {
+            anchored = false;
+        }
     }
 }
