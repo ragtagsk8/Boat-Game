bc155abee6c9458dd147a15f7445977b2c5f689f
ragtagsk8 <kanearriagada06@gmail.com>
2026-06-04 16:14:15 +1000

    procedurally generating rocks and islands

--- My project/Assets/Scripts/RandomWalkMapGenerator.cs
+++ My project/Assets/Scripts/RandomWalkMapGenerator.cs
@@ -11,8 +11,8 @@ public class RandomWalkMapGenerator : MonoBehaviour
 
     [SerializeField] protected Vector2Int startPosition =  Vector2Int.zero;
 
-    [SerializeField] private int iterations = 10;
-    [SerializeField] public int walkLength = 10;
+    [SerializeField] private int iterations = 2500;
+    [SerializeField] public int walkLength = 120;
     [SerializeField] public bool startRandomlyEachIteration = true;
 
     [SerializeField]
@@ -27,6 +27,13 @@ public class RandomWalkMapGenerator : MonoBehaviour
 
         tilemapVisualiser.Clear();
         tilemapVisualiser.PaintOceanTiles(floorPositions);
+
+        List<PointOfInterest> pointsOfInterest = GeneratePointsOfInterest(floorPositions);
+
+        foreach (var poi in pointsOfInterest) {
+            Debug.Log("POI: " + poi.type + " at " + poi.center);
+            GeneratePointOfInterest(poi, floorPositions);
+        }
     }
 
     protected HashSet<Vector2Int> RunRandomWalk() {
@@ -42,8 +49,183 @@ public class RandomWalkMapGenerator : MonoBehaviour
         return floorPositions;
     }
 
-    // new code
+    // POI generation code
+    [SerializeField] private int islandCount = 10;
+    [SerializeField] private int islandWalkLength = 30;
+    [SerializeField] private int islandIterations = 8;
+    [SerializeField] private int minPOISeparation = 15;
+    [SerializeField] private int islandEdgePadding = 15;
+    [SerializeField] private int spawnSafeRadius = 12;
 
+    public enum POIType {
+        Island, //25
+        Rock, //45
+        VillageIsland, //10
+        Shipwreck, //3
+        Crate, //7
+        Shoal, //10
+
+    }
+    public struct PointOfInterest {
+        public Vector2Int center;
+        public POIType type;
+
+        public PointOfInterest(Vector2Int center, POIType type) {
+            this.center = center;
+            this.type = type;
+        }
+    }
+
+    private List<PointOfInterest> GeneratePointsOfInterest(HashSet<Vector2Int> oceanPositions) {
+        List<Vector2Int> centers = PickIslandCenters(
+            oceanPositions,
+            islandCount,
+            minPOISeparation,
+            islandEdgePadding
+        );
+
+        List<PointOfInterest> points = new List<PointOfInterest>();
+
+        foreach (var center in centers) {
+            POIType type = GetRandomPOIType();
+            points.Add(new PointOfInterest(center, type));
+        }
+
+        return points;
+    }
+
+    private POIType GetRandomPOIType() {
+        int random = Random.Range(0, 99);
+        if (random >= 0 && random < 25) {
+            return POIType.Island;
+        } else if (random >= 25 && random < 70) {
+            return POIType.Rock;
+        } else if (random >= 70 && random < 80) {
+            return POIType.VillageIsland;
+        } else if (random >= 80 && random < 83) {
+            return POIType.Shipwreck;
+        } else if (random >= 83 && random < 90) {
+            return POIType.Crate;
+        } else /*if (random >= 90 && random < 100)*/ {
+            return POIType.Shoal;
+        }
+    }
+
+    private List<Vector2Int> PickIslandCenters(
+    HashSet<Vector2Int> oceanPositions,
+    int islandCount,
+    int minDistanceBetweenIslands,
+    int edgePadding
+) {
+        List<Vector2Int> centers = new List<Vector2Int>();
+        List<Vector2Int> possiblePositions = oceanPositions.ToList();
+
+        int attempts = 0;
+        int maxAttempts = islandCount * 100;
+
+        while (centers.Count < islandCount && attempts < maxAttempts) {
+            attempts++;
+
+            Vector2Int candidate = possiblePositions[Random.Range(0, possiblePositions.Count)];
+
+            if (Vector2Int.Distance(candidate, startPosition) < spawnSafeRadius)
+                continue;
+            if (!IsFarFromEdge(oceanPositions, candidate, edgePadding))
+                continue;
+
+            bool tooClose = false;
+
+            foreach (var center in centers) {
+                if (Vector2Int.Distance(candidate, center) < minDistanceBetweenIslands) {
+                    tooClose = true;
+                    break;
+                }
+            }
+
+            if (!tooClose) {
+                centers.Add(candidate);
+            }
+        }
+
+        return centers;
+    }
+    private bool IsFarFromEdge(HashSet<Vector2Int> positions, Vector2Int center, int padding) {
+        for (int x = -padding; x <= padding; x++) {
+            for (int y = -padding; y <= padding; y++) {
+                Vector2Int checkPos = center + new Vector2Int(x, y);
+
+                if (!positions.Contains(checkPos)) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    // POI Generation Code
+    private void GeneratePointOfInterest(PointOfInterest poi, HashSet<Vector2Int> oceanPositions) {
+        if (poi.type == POIType.Island) {
+            GenerateIsland(poi.center, oceanPositions);
+        } else if (poi.type == POIType.Rock) {
+            GenerateRock(poi.center, oceanPositions);
+        } else {
+            Debug.Log("No generation code yet for: " + poi.type);
+        }
+    }
+
+    private void GenerateRock(Vector2Int center, HashSet<Vector2Int> oceanPositions) {
+        if (oceanPositions.Contains(center)) {
+            tilemapVisualiser.PaintRockTile(center);
+        }
+    }
+    private HashSet<Vector2Int> GenerateIsland(Vector2Int center, HashSet<Vector2Int> oceanPositions) {
+        HashSet<Vector2Int> islandPositions = new HashSet<Vector2Int>();
+
+        Vector2Int currentPosition = center;
+
+        for (int i = 0; i < islandIterations; i++) {
+            var path = ProceduralGenerationScript.SimpleRandomWalk(currentPosition, islandWalkLength);
+
+            foreach (var pos in path) {
+                if (oceanPositions.Contains(pos)) {
+                    islandPositions.Add(pos);
+                }
+            }
+
+            currentPosition = path.ElementAt(Random.Range(0, path.Count));
+        }
+
+        islandPositions = FillIslandHoles(islandPositions);
+        islandPositions = FillIslandHoles(islandPositions);
+        islandPositions = FillIslandHoles(islandPositions);
+        islandPositions = RemoveTinyBits(islandPositions);
+        islandPositions = RemoveTinyBits(islandPositions);
+
+        tilemapVisualiser.PaintLandTiles(islandPositions);
+
+        return islandPositions;
+    }
+    private HashSet<Vector2Int> FillIslandHoles(HashSet<Vector2Int> islandPositions) {
+        HashSet<Vector2Int> result = new HashSet<Vector2Int>(islandPositions);
+
+        foreach (var position in GetBoundsPositions(islandPositions)) {
+            if (islandPositions.Contains(position))
+                continue;
+
+            int cardinalNeighbours = CountCardinalNeighbours(islandPositions, position);
+            int allNeighbours = CountNeighbours(islandPositions, position);
+
+            if (cardinalNeighbours >= 2 && allNeighbours >= 5) {
+                result.Add(position);
+            }
+        }
+
+        return result;
+    }
+
+
+    // New Ocean Generation Code
     private HashSet<Vector2Int> SmoothPositions(HashSet<Vector2Int> positions, int passes) {
         HashSet<Vector2Int> result = new HashSet<Vector2Int>(positions);
 
--- My project/Assets/Scripts/TilemapVisualiser.cs
+++ My project/Assets/Scripts/TilemapVisualiser.cs
@@ -13,17 +13,38 @@ public class TilemapVisualiser : MonoBehaviour
         oceanTilemap.RefreshAllTiles();
     }
 
+    [SerializeField] private Tilemap landTilemap;
+    [SerializeField] private TileBase landTiles;
+
+    public void PaintLandTiles(IEnumerable<Vector2Int> floorPositions) {
+        PaintTiles(floorPositions, landTilemap, landTiles);
+        landTilemap.RefreshAllTiles();
+    }
+
+    [SerializeField] private Tilemap rockTilemap;
+    [SerializeField] private TileBase rockTiles;
+
+    public void PaintRockTile(Vector2Int position) {
+        PaintSingleTile(rockTilemap, rockTiles, position);
+        rockTilemap.RefreshAllTiles();
+    }
+
     private void PaintTiles(IEnumerable<Vector2Int> positions, Tilemap tilemap, TileBase tile) {
         foreach (var position in positions) {
             PaintSingleTile(tilemap, tile, position);
         }
     }
-    private void PaintSingleTile(Tilemap tilemap, TileBase tile, Vector2Int position) {
+    /*private void PaintSingleTile(Tilemap tilemap, TileBase tile, Vector2Int position) {
         var tilePosition = tilemap.WorldToCell((Vector3Int)position);
         tilemap.SetTile(tilePosition, tile);
+    }*/
+    private void PaintSingleTile(Tilemap tilemap, TileBase tile, Vector2Int position) {
+        tilemap.SetTile((Vector3Int)position, tile);
     }
 
     public void Clear() {
         oceanTilemap.ClearAllTiles();
+        landTilemap.ClearAllTiles();
+        rockTilemap.ClearAllTiles();
     }
 }
